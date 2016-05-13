c7e8d8d3f019c17c469d876f538c315e3690eabe
David Weinstein <dweinst@insitusec.com>
2016-05-13 12:32:34 -0400

    Refactored and clean up API
    - Update README to match new API
    - use `macho-is-encrypted`
    - close #2

--- bin.js
+++ bin.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 'use strict'
-const extractExec = require('./index')
+const extractExec = require('./')
 const fs = require('fs')
+const encryptedSync = require('macho-is-encrypted').path
 
 if (!process.argv[2]) {
   console.error(`${process.argv[1]} <path>`)
@@ -10,7 +11,13 @@ if (!process.argv[2]) {
 
 const fd = fs.openSync(process.argv[2], 'r')
 
-extractExec(fd, (err, path, fd, name, plist, encrypted, cleanupCb) => {
+const ENC_DESC = {
+  true: 'encrypted',
+  false: 'not encrypted'
+}
+
+extractExec(fd, (err, tmpPath, name, plist, origFile, cleanupCb) => {
   if (err) { throw err }
-  console.log(`${name} is ${encrypted ? 'encrypted' : 'not encrypted'}`)
+  const encrypted = encryptedSync(tmpPath)
+  console.log(`${name} (${plist.CFBundleIdentifier}) is ${ENC_DESC[encrypted]}`)
 })
